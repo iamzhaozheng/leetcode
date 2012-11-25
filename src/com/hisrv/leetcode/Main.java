c17619e931a1afa7381716819af426979d195caa
Zhao Zheng <zhengzhaomail@gmail.com>
2012-11-25 11:11:05 +0800

    palindrome

--- src/com/hisrv/leetcode/Main.java
+++ src/com/hisrv/leetcode/Main.java
@@ -1,12 +1,10 @@
 package com.hisrv.leetcode;
 
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<String> list = new LetterCombinationOfAPhoneNumber().letterCombinations("2");
-		for (String s : list) {
-			System.out.println(s);
-		}
+		LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+		System.out.print(l.longestPalindrome("ccc"));
 	}
+	
 }
