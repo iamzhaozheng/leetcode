768c4922459d7b8f0e71c9521d83d3bb211c974d
Zheng Zhao <zhengzhaomail@gmail.com>
2012-12-07 18:17:41 +0800

    add one

---
+++ src/com/hisrv/leetcode/MergeKSortedList.java
@@ -0,0 +1,27 @@
+package com.hisrv.leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * Definition for singly-linked list.<br>
+ * public class ListNode {<br>
+ *     int val;<br>
+ *     ListNode next;<br>
+ *     ListNode(int x) {<br>
+ *         val = x;<br>
+ *         next = null;<br>
+ *     }<br>
+ * }<br>
+ */
+public class MergeKSortedList {
+    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode ret = null;
+        int n = lists.size();
+        if (n == 0) {
+        	return ret;
+        }
+        return ret;
+    }
+}
