9120bc50932a26fb7c46fa18df2a20a4ff939c64
Zheng Zhao <zhengzhaomail@gmail.com>
2014-04-25 15:02:24 +0800

    commit

--- src/com/hisrv/leetcode2/TwoSum.java
+++ src/com/hisrv/leetcode2/TwoSum.java
@@ -5,45 +5,38 @@ import java.util.Comparator;
 
 public class TwoSum {
     public int[] twoSum(int[] numbers, int target) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int n = numbers.length;
-        int[] ret = new int[2];
-        if (n < 2) {
-            return null;
+        Bunch[] bunches = new Bunch[numbers.length];
+        for (int i = 0; i < numbers.length; i ++) {
+            bunches[i] = new Bunch(numbers[i], i + 1);
         }
-        Num[] num = new Num[n];
-        for (int k = 0; k < n; k ++) {
-            num[k] = new Num(numbers[k], k + 1);
-        }
-        Arrays.sort(num, new Comparator<Num> () {
-            public int compare(Num a, Num b) {
-                return b.v - a.v;
-            }
-        });
-        int i = 0, j = n - 1;
+        Arrays.sort(bunches, new Comparator<Bunch> () {
+                public int compare(Bunch a, Bunch b) {
+                    return a.num - b.num;
+                }
+            });
+        int i = 0;
+        int j = numbers.length - 1;
+        
         while (i < j) {
-            int sum = num[i].v + num[j].v;
-            if (sum < target) {
+            int sum = bunches[i].num + bunches[j].num;
+            if (sum > target) {
                 j --;
-            } else if (sum > target) {
+            } else if (sum < target) {
                 i ++;
             } else {
-                ret[0] = num[i].idx;
-                ret[1] = num[j].idx;
-                break;
+                int[] ret = new int[] {bunches[i].idx, bunches[j].idx};
+                Arrays.sort(ret);
+                return ret;
             }
         }
-        Arrays.sort(ret);
-        return ret;
+        return new int[] {0, 0};
     }
     
-    private class Num {
-        int v;
-        int idx;
-        
-        public Num(int v, int idx) {
-            this.v = v;
+    private class Bunch {
+        public int num;
+        public int idx;
+        public Bunch(int num, int idx) {
+            this.num = num;
             this.idx = idx;
         }
     }
---
+++ src/com/hisrv/leetcode3/TwoSum.java
@@ -0,0 +1,23 @@
+package com.hisrv.leetcode3;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        Arrays.sort(numbers);
+        int i = 0;
+        int j = numbers.length - 1;
+        
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum > target) {
+                j --;
+            } else if (sum < target) {
+                i ++;
+            } else {
+                return new int[] {i + 1, j + 1};
+            }
+        }
+        return new int[] {0, 0};
+    }
+}
